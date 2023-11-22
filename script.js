75f2cfecb9338b507611c39fc1b483cabbc6b154
Naa Shidaa <rainaaaddo@gmail.com>
2023-11-22 11:21:30 +0000

    Update index.html

--- script.js
+++
@@ -1,4 +0,0 @@
-function toggleSearchBar() {
-    var searchInput = document.getElementById("searchInput");
-    searchInput.style.display = (searchInput.style.display === "none") ? "inline-block" : "none";
-}
